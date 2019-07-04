d18aa56e98a4df2bee83c91d1a36a672231210e1
waka <liangnida@163.com>
2019-07-04 19:59:58 +0800

    fine

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 cd el_2019
 export PYTHONPATH="."
 export ROOT_DIR="root"
-/root/anaconda3/envs/py36/bin/python prtrain_LM/process_pretrain_data.py
+/root/anaconda3/envs/py36/bin/python pretrain_LM/process_pretrain_data.py
