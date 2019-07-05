3fe2bd07c91c3339897142cc93f8998063850b4a
waka <liangnida@163.com>
2019-07-05 19:33:43 +0800

    subject

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ cd el_2019
 export PYTHONPATH="."
 export ROOT_DIR="root"
 #/root/anaconda3/envs/py36/bin/python pretrain_LM/finetune_on_pregenerated.py
-/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_object.py
+#/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_object.py
+/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_subject.py
