e4040d782147d1c6c2a0dd0f8c5d7561c2bb18a5
waka <liangnida@163.com>
2019-07-07 16:04:05 +0800

    subject

--- run.sh
+++ run.sh
@@ -6,11 +6,18 @@ export ROOT_DIR="root"
 
 #/root/anaconda3/envs/py36/bin/python pretrain_LM/finetune_on_pregenerated.py
 
-#/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_object.py
+
 
 #/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_subject.py
 
-/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_subject_eval.py
+#/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_subject_eval.py
+
+/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_subject_submit.py
+
+
+
+
+#/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_object.py
 
 #/root/anaconda3/envs/py36/bin/python baseline_3/el_pt_object_submit.py
 
