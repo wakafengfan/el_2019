782edaf6bc6e691465eeaddd055d3ac15bf17c55
waka <liangnida@163.com>
2019-07-07 11:09:21 +0800

    object_eval

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ export ROOT_DIR="root"
 #/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_object.py
 #/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_subject.py
 #/root/anaconda3/envs/py36/bin/python baseline_3/el_pt_object_submit.py
-/root/anaconda3/envs/py36/bin/python baseline_3/el_pt_object_eval.py
+/root/anaconda3/envs/py36/bin/python baseline_4/el_pt_object_eval.py
